89e121716e29701d0d36c3921781c54a0ba9f003
rayhanramadhan17 <rayhanramadhan179072@gmail.com>
2026-05-11 18:46:08 +0700

    menyertakan file header jantung h dan manusia h

--- komposisi.cpp
+++ komposisi.cpp
@@ -6,11 +6,3 @@ using namespace std;
 #include "jantung.h"
 #include "manusia.h"
 
-int main()
-{
-    //deklarasi dan pemberian nilai object pointer varManusia
-    Manusia* varManusia = new Manusia("jono");
-    //mendelete pointer varManusia dari memori
-    delete varManusia;
-    return 0;
-}
