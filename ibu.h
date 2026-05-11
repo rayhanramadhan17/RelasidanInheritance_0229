2b0c3adf2398892b1b37c32e495786f6545f07b9
rayhanramadhan17 <rayhanramadhan179072@gmail.com>
2026-05-11 18:35:30 +0700

    deklarasi member variabel class ibu

--- ibu.h
+++ ibu.h
@@ -10,32 +10,4 @@ public:
     string nama;
     vector<anak*> daftar_anak;
 
-    //membuat constructor dan destructor pada class ibu
-    Ibu(string pNama) :nama(pNama) {
-        cout << "Ibu \"" << nama << "\" ada\n";
-    };
-
-    ~Ibu() {
-        cout << "Ibu \"" << nama << "\" tidak ada\n";
-    };
-
-    //deklarasi prosedur tambahAnak() dan cetakAnak()
-    void tambahAnak(anak* pAnak);
-    void cetakAnak();
-};
-
-//pendefinisian prosedur - prosedur pada class ibu diluar class
-void Ibu::tambahAnak(anak* pAnak) {
-    daftar_anak.push_back(pAnak);
-}
-
-void Ibu::cetakAnak() {
-    cout << "Daftar Anak dari Ibu \"" << this->nama << "\":\n";
-    for (auto& a : daftar_anak)
-    {
-        cout << a->nama << "\n";
-    }
-    cout << endl;
-}
-
-#endif
+ 
