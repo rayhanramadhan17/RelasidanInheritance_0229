e3ce2021dbec8377bd834db9d51b7d4122ea8880
rayhanramadhan17 <rayhanramadhan179072@gmail.com>
2026-05-11 18:36:10 +0700

    deklarasi prosedur tambahanak dan cetak anak

--- ibu.h
+++ ibu.h
@@ -10,4 +10,17 @@ public:
     string nama;
     vector<anak*> daftar_anak;
 
- 
+    //membuat constructor dan destructor pada class ibu
+    Ibu(string pNama) :nama(pNama) {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    };
+
+    ~Ibu() {
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
+    };
+
+    //deklarasi prosedur tambahAnak() dan cetakAnak()
+    void tambahAnak(anak* pAnak);
+    void cetakAnak();
+};
+
