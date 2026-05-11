a43190c8daa0cf8334decc38b75ac0293266e84e
rayhanramadhan17 <rayhanramadhan179072@gmail.com>
2026-05-11 18:43:24 +0700

    membuat class jantung di file header jantung h

--- jantung.h
+++ jantung.h
@@ -4,13 +4,3 @@
 //membuat class jantung di file header jantung.h
 class Jantung {
 public:
-    //membuat constructor dan destructor dari class jantung
-    Jantung() {
-        cout << "Jantung Ditambah\n";
-    }
-    ~Jantung() {
-        cout << "Jantung Dimatikan\n";
-    }
-};
-
-#endif // !JANTUNG_H
